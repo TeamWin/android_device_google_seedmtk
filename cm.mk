b1776e8df11c0128314b87a33236def38c775aa7
Varun Chitre <varun.chitre15@gmail.com>
2015-10-25 00:58:51 -0400

    sprout4: update fingerprint
    Change-Id: I91046788f9a16c8a00215cc0aae158747eeb2089

--- cm.mk
+++ cm.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/google/sprout4/sprout4.mk)
 # Common CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1/LMY47O/1783956:user/release-keys PRIVATE_BUILD_DESC="sprout-user 5.1 LMY47O 1783956 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
 PRODUCT_NAME := cm_sprout4
 PRODUCT_DEVICE :=sprout4
