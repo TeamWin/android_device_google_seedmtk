6db4afb746e53a448d48b125897c2965217db317
MSF Jarvis <msfjarvis@gmail.com>
2016-04-06 19:16:02 +0000

    Revert "In reference to @035ea2f"

---
+++ seedmtk.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# common sprout
+$(call inherit-product, device/mediatek/sprout-common/sprout.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/root/init.sprout.rc:root/init.sprout.rc \
+    $(LOCAL_PATH)/rootdir/root/fstab.sprout:root/fstab.sprout	\
+#    $(LOCAL_PATH)/rootdir/root/kernel:kernel
